0c5c5ac646dab5041fbb9e02ce209ef0ff1097b9
unknown <fon0948630361@gmail.com>
2020-08-18 23:13:09 +0700

    Up

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div  id="show">
+    <div v-if="show">
+      <div class="card-body">
+        <ul class="list-inline">
+          <li v-for="item in items" :key="item" class="list-inline-item">
+            {{item.counter}}
+            <btn :label="item.title" v-on:click="console.log('hehs')" />
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <button @mouseover="show=true" @mouseleave="show=false">like</button>
+    </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import btn from "@/components/Button.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    btn,
+  },
+  data() {
+    return {
+      show: false,
+      items: [
+        { id: 1, title: "like", counter: 0 },
+        { id: 2, title: "love", counter: 0 },
+        { id: 4, title: "haha", counter: 0 },
+        { id: 5, title: "wow", counter: 0 },
+        { id: 6, title: "angry", counter: 0 },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -24,5 +50,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#show{
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  
+}
+
+#show > * {
+  grid-area:1/1/1/1;
+
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button>{{label}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label:String
+    }
+}
+</script>
+
+<style>
+
+</style>
